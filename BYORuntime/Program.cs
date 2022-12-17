988c75c2d6693062ce6fe40be644962da1612555
Pankaj Chaurasia <krishnahare14@gmail.com>
2022-12-17 17:06:19 +0530

    Implementing virtual functions

--- BYORuntime/Program.cs
+++ BYORuntime/Program.cs
@@ -1,16 +1,52 @@
 ﻿Report(new Base());
 Report(new Derived());
 
-void Report(Base b) => Console.WriteLine($"{b.WhoAmI(),10} - {b.WhatCanIDo()}");
+void Report(Base b) => Console.WriteLine($"{b.WhoAmI(),10} - {Runtime.Call<string>(b,"WhatCanIDo")}");
 
-class Base
+
+delegate object virtual_function(params object[] args);
+
+static class Runtime
 {
+    public static T Call<T>(ImplicitLayout instance, string functionName, params object[] args)
+    {
+        virtual_function function = instance._metadata._vtable[functionName];
+        return (T)function.Invoke(args);
+    }
+}
+
+class TypeMetadata
+{
+    internal Dictionary<string, virtual_function> _vtable = new();
+
+}
+
+class ImplicitLayout
+{
+    internal TypeMetadata _metadata = new();
+
+    protected void Register<T>(string functionName, Func<T> f) =>
+        _metadata._vtable[functionName] = f.DynamicInvoke;
+}
+
+class Base : ImplicitLayout
+{
+    public Base() : base()
+    {
+        Register<string>("WhatCanIDo", () => "I do stuff");
+    }
+
+    // _metadata comes here
     public virtual string WhoAmI() => this.GetType().Name;
-    public virtual string WhatCanIDo() => "I can do this";
 }
 
 
 class Derived : Base
 {
-    public override string WhatCanIDo() => "I can do more!!";
+    public Derived() : base()
+    {
+        Register<string>("WhatCanIDo", () => "I can do more!!");
+
+    }
+    // _metadata comes here
 }
