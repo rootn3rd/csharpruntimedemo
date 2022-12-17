45c3accb7cf218abf6a71a3e01a02da4dbde5077
Pankaj Chaurasia <krishnahare14@gmail.com>
2022-12-17 16:48:48 +0530

    Initial commit

---
+++ BYORuntime/Program.cs
@@ -0,0 +1,16 @@
+﻿Report(new Base());
+Report(new Derived());
+
+void Report(Base b) => Console.WriteLine($"{b.WhoAmI(),10} - {b.WhatCanIDo()}");
+
+class Base
+{
+    public virtual string WhoAmI() => this.GetType().Name;
+    public virtual string WhatCanIDo() => "I can do this";
+}
+
+
+class Derived : Base
+{
+    public override string WhatCanIDo() => "I can do more!!";
+}
